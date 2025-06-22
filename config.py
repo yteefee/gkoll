4e8da340a7b3e949b5fdd037815651761dbd883d
yteefee <yteefee@mail.ru>
2025-06-22 15:43:23 +0700

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x] or [123456789]
