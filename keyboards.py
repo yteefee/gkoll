e60cefcc5336d7748f1fe7172c1fb05f20823977
yteefee <yteefee@mail.ru>
2025-06-22 12:24:52 +0700

    Delete keyboards.py

--- keyboards.py
+++
@@ -1,8 +0,0 @@
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
-
-def base_keyboard():
-    builder = ReplyKeyboardBuilder()
-    builder.button(text="Статистика")
-    builder.button(text="Помощь")
-    builder.adjust(2)
-    return builder.as_markup(resize_keyboard=True)
